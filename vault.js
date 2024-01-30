f501c8bb66fb9fc7e0e4f4f71b8d6ec1ccef97b6
David McHugh Thompson <dmthompson925@gmail.com>
2024-01-30 12:18:10 -0500

    the first of many

---
+++ vault.js
@@ -0,0 +1,10 @@
+//PSUEDOCODE
+// 1. Build the requisite github repository and clone it to my computer
+// 2. Create the necessary files (js,html,readme)
+// 3. Write Psuedocode (Woah man)
+// 4. Create three equations (3 distinct types of mathematical operators) (not hard values) that produce the numbers 10 - 40 - 39
+// 5. Display those numbers in an alert popup OR an html page (figure out how to do both)
+// 6. make a string that says "You have received this message because you have been chosen to open an important vault. Here is the secret combination:" and concatenate it with the numbers (which will be the product of the three equations you make) (make sure to use backticks to make things easier and avoid having to hard code the variable numbers)
+// 7. Figure out how to display that fully concatenated string on a page, either via html or an alert
+// 8. Sprinkle some comments throughout, explaining what each moving part does and why it exists
+// 9.Sit back and watch the adoration and adulation for this articulated and inimitable display of coding genius
